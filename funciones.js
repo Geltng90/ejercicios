326b614037b73b2472e3dc7dd1209b28cc55f6bb
Geltng90 <160567009+Geltng90@users.noreply.github.com>
2024-03-12 21:25:01 -0600

    Add files via upload

---
+++ funciones.js
@@ -0,0 +1,8 @@
+function saludar()
+const nombre = "UNID";
+console.log ("Hola " , nombre);
+
+
+function saludarDos  ()
+const nombreDos = prompt ("Ingresa un nombre");
+const.log ("Hola", nombreDos)
